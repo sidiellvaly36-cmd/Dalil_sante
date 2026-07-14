f1db220930e4b5ae1023abd8729e2089e41ba357
Sidi Ellvaly <sidiellvaly36@gmail.com>
2026-07-14 14:32:53 +0000

    Initial Spring Boot backend setup

---
+++ Backend/src/main/java/com/Stage/Dalil_sante/DalilSanteApplication.java
@@ -0,0 +1,13 @@
+package com.Stage.Dalil_sante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalilSanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DalilSanteApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/Stage/Dalil_sante/DalilSanteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Stage.Dalil_sante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalilSanteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
